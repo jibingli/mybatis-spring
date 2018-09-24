a203b7b05f636da7683813e04072150a133af665
wz <296215200@qq.com>
2018-09-24 21:01:28 +0800

    mybatis generator

---
+++ src/main/java/com/enjoylearning/mybatis/entity/BaseEntity.java
@@ -0,0 +1,14 @@
+package com.enjoylearning.mybatis.entity;
+
+import java.io.Serializable;
+
+/**
+ * @Author : Wang Zhen.
+ * @Date : Created in 20:43 2018/9/24
+ * @Description :
+ * @Modified By   :
+ * @Version :
+ */
+public class BaseEntity implements Serializable {
+
+}
